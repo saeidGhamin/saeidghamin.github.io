60fcab7eb20c69ba6913725642e8ef0fc743a1d9
saeidGhamin <saeidghamin@gmail.com>
2025-04-28 16:19:41 +0330

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+
+// انیمیشن ساده هنگام اسکرول
+const faders = document.querySelectorAll('.fade-in');
+
+const appearOptions = {
+  threshold: 0.2,
+  rootMargin: "0px 0px -50px 0px"
+};
+
+const appearOnScroll = new IntersectionObserver(function(entries, appearOnScroll) {
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) {
+      return;
+    } else {
+      entry.target.classList.add('visible');
+      appearOnScroll.unobserve(entry.target);
+    }
+  });
+}, appearOptions);
+
+faders.forEach(fader => {
+  appearOnScroll.observe(fader);
+});
